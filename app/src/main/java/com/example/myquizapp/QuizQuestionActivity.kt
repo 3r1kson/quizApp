1acd570516d5cc202ace6001360fde973ffaee67
Erikson <eriksonribeiro84@gmail.com>
2024-10-28 23:51:00 -0300

    first commit

---
+++ app/src/main/java/com/example/myquizapp/QuizQuestionActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myquizapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class QuizQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+
+    }
+}
